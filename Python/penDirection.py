8c7dd0469ddf34974c1488ee3fb3716f2d62dd5a
pgadani <pgadani123@gmail.com>
2017-10-14 23:45:12 -0400

    Need to test serial stuff

---
+++ Python/penDirection.py
@@ -0,0 +1,11 @@
+
+gravity = []
+accel = []
+gyro = []
+
+def visualize(gyro, accel, gravity) {
+	# Gravity is in relation to the world: (0 0 -g)
+	orientation = []
+	for g, a in zip(accel, gyro):
+		
+}
--- Python/serialTest.py
+++ Python/serialTest.py
@@ -7,6 +7,14 @@ if __name__ == '__main__':
 	s = serial.Serial(port=port, baudrate=baud)
 	while(True):
 		if s.readable():
-			msg = [ord(c) for c in s.read(12)]
-			a = [(128-msg[0]) * 256 + msg[1], (128-msg[2]) * 256 + msg[3], (128-msg[4]) * 256 + msg[5]]
-			print('ax: ', a[0], 'ay', a[1], 'az', a[2])
+			# msg = [ord(c) for c in s.read(12)]
+			# a = [(128-msg[0]) * 256 + msg[1], (128-msg[2]) * 256 + msg[3], (128-msg[4]) * 256 + msg[5]]
+			# print('ax: ', a[0], 'ay', a[1], 'az', a[2])
+			msg = s.read(12) # read 6 values, 2 bytes each
+			gyro = []
+			accel = []
+			for a in range(0, 6, 2):
+				gyro.append(int.from_bytes(msg[a:a+2], byteorder='big'))
+				accel.append(int.from_bytes(msg[a+6:a+8], byteorder='big'))
+			print("Gyro: ", gyro)
+			print("Accel: ", accel)
